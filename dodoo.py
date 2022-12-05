10d52592d6b3a0044fe222ed65fab30c2f2587e5
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-05 23:34:08 +0530

    Concathinating string and int

---
+++ dodoo.py
@@ -0,0 +1,6 @@
+charecter_name = "Devu"
+charecter_age = 343
+print("There once was a man named George" + charecter_name + ", ")
+print("he was " + str(charecter_age)  + " yers old. ")
+print("He really liked the name george" + charecter_name + ", ")
+print("but he didn't liked being " + str(charecter_age) + " .")
