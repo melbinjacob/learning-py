a1217d565dfe442aea9f438f3d955e4c0d3916af
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-06 22:45:39 +0530

    Age and name app

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-user_name = input("Hey what is your name?")
-print("Hello " + user_name)
+user_name = input("Hey what is your name? ")
+age = ( input("How old are you " + user_name +  " ? "))
+print("Hello " + user_name + ", You are " + age + " Years old!")
