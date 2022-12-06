1843d2a9651f64f0177e1321fc04141c17a78efa
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-06 10:29:33 +0530

    Lerning variables

--- dodoo.py
+++
@@ -1,6 +0,0 @@
-charecter_name = "Devu"
-charecter_age = 343
-print("There once was a man named George" + charecter_name + ", ")
-print("he was " + str(charecter_age)  + " yers old. ")
-print("He really liked the name george" + charecter_name + ", ")
-print("but he didn't liked being " + str(charecter_age) + " .")
