f06dc2a7507a64cea2caa4cf8b1448c06d301369
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-07 10:37:48 +0530

    Inserting value to a list

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-hello = [
-    "H",#0
-    "e",#1
-    "l",#2
-    "l",#3
-    "o",#4
-    " ",#5
-    "w",#6
-    "o",#7
-    "r",#8
-    "l",#9
-    "d",#10
-]
-print(hello[:5])
-print(hello[6:11])
-print(hello[2:5])
-print(hello[1:8])
-print(hello[0])
+change = [1 , 2 ,4 ,5]
+change.insert(2, 3)
+print(change)   
