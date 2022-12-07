ca34411b151fd471e3bb67258349493679640e93
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-07 21:26:19 +0530

    Hotel menu

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
-num_1 = input("Enter a number: ")
-num_2 = input("Enter another number :")
-result = float(num_1) + float(num_2)
+menu = [
+    "Mutta",
+    "Biriyani",
+    "Dosa",
+    "Appam",
+    "Puttu",
+    "Idiyappam",
+    "Poori"
+] 
+print(menu)
 
-print(result)
+item = int(input("Enter a number : "))
+
+if item == 1:
+    print(menu[0])
+elif item == 2:
+    print(menu[1])
+elif item == 3:
+    print(menu[2])
+elif item == 4:
+    print(menu[5])
+elif item == 5:
+    print(menu[6])
+else :
+    print("Invalid")
