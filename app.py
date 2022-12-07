263a62aa03b02f1c90518ba18cb5f0b593a8efeb
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-07 10:30:29 +0530

    Slicing

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-#my name letter variable
-my_letters = [
-    "m",
-    "e",
-    "l",
-    "b",
-    "i",
-    "n"
+hello = [
+    "H",#0
+    "e",#1
+    "l",#2
+    "l",#3
+    "o",#4
+    " ",#5
+    "w",#6
+    "o",#7
+    "r",#8
+    "l",#9
+    "d",#10
 ]
-print(my_letters[1:4])
-print(my_letters[:-1])
-print(my_letters[0:])
+print(hello[:5])
+print(hello[6:11])
+print(hello[2:5])
+print(hello[1:8])
+print(hello[0])
