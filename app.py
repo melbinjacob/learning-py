81fa4c473e63e561bb0ac3cb33c71c7f5fa7053e
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-09 10:18:13 +0530

    if, elif, else statements in python

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-a_dictnory = dict(
-    Name = "Melbin",
-    Year = 2002,
-    Male = True
-)
 
-a_dictnory["Name"] = "Arun"
+a = input("Enter a number (A):")
+b = input("Enter another number (B) :")
 
-print(a_dictnory)
+if a > b:
+    print("A is grater than B")
+elif a == b:
+    print("A and B are equal")
+else:
+    print("A is not greater than B")
