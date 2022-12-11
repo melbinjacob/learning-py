923b8a376cdb69fc075ab968ad0665197a6a4892
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 12:31:49 +0530

    Return statements in python function

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #Return statements in python functions
 
 def cube(num):
+        print("This will get printed")
         return num*num*num
+        Print("This will not print onto the terminal because the function exited after the return stement")
 value = cube(100)
 print(value)
