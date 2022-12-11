7b9171d606aa4a7c60c5266ad3a9b8fb99ba4b0c
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 13:23:29 +0530

    Comparison operators in python if else

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-is_male = False
-is_tall = False
+def larger_number(num1, num2, num3):
+        if num1 >= num2 and num1 >= num3:
+          return  num1
+        elif num2 >= num1 and num2 >= num3:
+                return num2
+        else:
+                return num3
 
-if is_male and is_tall:
-        print("You are a tall male")
-elif is_male and not(is_tall):
-        print("You are a short male")
-elif not(is_male) and is_tall:
-        print("You are a tall female")
-else:
-        print("You a short female")
+print(larger_number(100, 10, 4))
