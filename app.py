bd57f13c6e288d4fde94d40e285cb44b83247037
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 10:19:32 +0530

    Lists Recap

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-a = 60; 
-if a > 10:
-    print("A is grater than 10")
-if a > 20:
-    print("A is also grater than 20")
-if a > 30:
-    print("A is also grater than 30")
-if a > 50:
-    print("A is also grater than 50")
+friends = [
+        "Arun",
+        "Ajith",
+        "Abijith",
+        "Alen",
+        "Bonny"
+]
+
+print(friends)
