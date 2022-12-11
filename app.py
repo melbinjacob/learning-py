8ad0c5e45428f7ea7c7084aaf6b2eeca295fdb60
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 13:05:44 +0530

    If elif else with and

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-is_male =False
-is_tall = True
+is_male = False
+is_tall = False
 
-if is_male or is_tall:
-        print("You are a male or tall or both")
+if is_male and is_tall:
+        print("You are a tall male")
+elif is_male and not(is_tall):
+        print("You are a short male")
+elif not(is_male) and is_tall:
+        print("You are a tall female")
 else:
-        print("You a female and not tall")
+        print("You a short female")
