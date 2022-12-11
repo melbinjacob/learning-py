df93500dcc4568f6f15c53ab155eebe07950b1a3
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 10:52:55 +0530

    .reverse function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 luckey_numbers =        [ 2, 4, 6, 8, 10, 12, 14, 16]
 friends = [ "Arun", "Abhijith", "Alen","Jesla", "Alen","Alen","Alen", "Bonny", "Yashi"]
 
-friends.sort()
+friends.reverse() #reversers the list
 print(friends)
