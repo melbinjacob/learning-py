644148cd753894d3557594f2bb6554c9d2942ced
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 10:43:08 +0530

    list.remove function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 luckey_numbers =        [ 2, 4, 6, 8, 10, 12, 14, 16]
 friends = [ "Arun", "Abhijith", "Alen", "Bonny", "Jesla", "Yashi"]
 
-friends.insert(1, "Varun")
+friends.remove("Jesla")
 print(friends)
