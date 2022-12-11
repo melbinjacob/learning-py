35130c8005939cd050ab5242e89804a62c1cbb11
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-11 10:47:36 +0530

    Get the index of an element from the list

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 luckey_numbers =        [ 2, 4, 6, 8, 10, 12, 14, 16]
 friends = [ "Arun", "Abhijith", "Alen", "Bonny", "Jesla", "Yashi"]
 
-friends.pop(1)
-print(friends)
+# friends.pop(1)
+print(friends.index("Alen"))
