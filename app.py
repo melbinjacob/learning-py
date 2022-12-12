03874ba927b75f7ef884081a220d8be1734a3f2d
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-12 09:54:48 +0530

    Guessing game using while loop in python

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-i = 1
-while i <= 10:
-        print(i)
-        i += 1
-print("Done with loop")
+secret_word = "melbin"
+
+guess = ""
+
+while guess != secret_word:
+        guess = input("I am you enter who am I : ")
+print("You won!")
