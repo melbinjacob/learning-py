fb5f17a00d0dd0c24198eb24fe9e0158ec094a17
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-12 09:52:16 +0530

    While loop  in python

--- app.py
+++ app.py
@@ -1,37 +1,5 @@
-mo_conv = {
-        "Jan" : "January",
-        "Feb" : "February",
-        "Mar" : "March",
-        "Apr" : "April",
-        "May" : "May",
-        "Jun" : "June",
-        "Aug" : "August",
-        "Sep" : "September",
-        "oct" : "October",
-        "Nov" : "November",
-        "Dec" : "December"
-}
-
-print(mo_conv["Dec"])
-print(mo_conv.get("Dec"))
-print(mo_conv.get("Lov", "Not a valid key"))
-
-nu_conv = {
-        1 : 10,
-        2 : 20,
-        3 : 30,
-        4 : 40,
-        5 : 50,
-        6 : 60,
-        7 : 70,
-        8 : 80,
-        9 : 90,
-        10 : 100
-}
-
-#you can store any type of key value pairs in dict
-rand = {
-        1 : True,
-        "True" : False,
-        False : 3.4,
-}
+i = 1
+while i <= 10:
+        print(i)
+        i += 1
+print("Done with loop")
