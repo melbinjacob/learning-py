16b3e53f274e5877aa3ce32ea91ab6bcf246272a
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-13 11:08:40 +0530

    Num converter

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-name1 = input("What is your name? ")
-person1 = int(input( name1 + " what's your age : "))
+num = float(input("Enter a number :"))
 
-name2 = input("What is your name ? ")
-person2 = int(input( name2 + " what's your age : "))
-
-name3 = input("What is your name ? ")
-person3 = int(input( name3 + " what's your age : "))
-
-if person1 > person2 and person1 > person3:
-        print(name1 + " is the oldest")
-elif person1 == person2 and person2 == person3:
-        print("All of you have the same age")
-elif person2 > person1 and person2 > person3:
-        print(name2 + " is the oldest")
+if num < 0 :
+        print(str(num * -1))
 else :
-        print(name3 + " is the oldest")
+        print(str(num))
