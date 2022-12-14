63d110b09f815857964ee7420df71eae624f7479
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-14 10:54:01 +0530

    Break in while loop

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 i = 1
-j = 1
-while i <= 5:
-        print("Someone ", end="")
-        while j <= 5:
-                print("rocks ", end="")
-                j += 1
+
+while i <= 10:
+        print("Some text" , i)
+        if i == 10:
+                break
         i+=1
-        j = 1
-        print()
