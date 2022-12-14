d4aff30b5a651ed9f4947dcfee15653256d935c4
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-14 10:28:00 +0530

    Nested while loop

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-classes_held = int(input("How many classes was held : "))
-classes_attented = int(input("How many classes did you attent : "))
-
-
-persentage = (classes_attented/classes_held) * 100
-
-if persentage < 75 :
-        print("You only have " + str(persentage) + "% Attentance You are not eligible for the exam")
-else:
-        print("You have " + persentage + "% Attentance you are eligible for the exam")
+i = 1
+j = 1
+while i <= 5:
+        print("Someone ", end="")
+        while j <= 5:
+                print("rocks ", end="")
+                j += 1
+        i+=1
+        j = 1
+        print()
