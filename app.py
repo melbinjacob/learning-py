9fd39b09786075f039448b695653be9f8ff60d4c
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-19 09:27:31 +0530

    FInd the sum of the elements in a list

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-num = input("Enter a number : ")
+list = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+]
+sum = 0
 
-count = 0
-for i in num:
-        count += 1
-print(count)
+
+for i in list :
+        sum = sum + i
+print(sum)
