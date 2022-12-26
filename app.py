bbc88d5ad55d10248062855e222f7cfb65b56b6b
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-26 11:44:05 +0530

    Python logical operators

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-x = 3 > 2
-print(x)
+price = 31
 
-x = 3 < 2
-print(x)
-
-x = 3==2
-print(x)
-
-x = 3!=2
-print(x)
+print(price > 10 and price < 30) #Both conditions needs to be true
+print(price > 10 or price < 30) #Only one condition needs to be true
+print(not(price > 10)) #Flips true or false 
