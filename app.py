447d48dbb9c9a27fe3e9e1d834086d413fb0d3c7
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-27 08:26:00 +0530

    Print star pyramid using while loop

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-weight = float(input("Enter your weight: "))
-unit = input("Kg (K) or Lbs (L): ")
-
-
-if unit.upper() == "L":
-    print("Your weight in kilograms is " + str((weight/0.45)))
-elif unit.upper() == "K":
-    print("Your weight in pounds is " + str((weight * 2.2)))
-else:
-    print("Invalid input")
+i = 1
+j = 10
+k = 0
+while i <= 10:
+    print(j* " ",end="")
+    print(i * "*",end="")
+    print(k * "*")
+    i += 1
+    j -= 1
+    k += 1
+print("Done")
