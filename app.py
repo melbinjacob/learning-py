1155b59b2e6a66e0c7bd903b4e0a621e5b1d02bd
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-27 07:49:00 +0530

    Weight calculator app

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-price = 31
+weight = float(input("Enter your weight: "))
+unit = input("Kg (K) or Lbs (L): ")
 
-print(price > 10 and price < 30) #Both conditions needs to be true
-print(price > 10 or price < 30) #Only one condition needs to be true
-print(not(price > 10)) #Flips true or false 
+
+if unit.upper() == "L":
+    print("Your weight in kilograms is " + str((weight/0.45)))
+elif unit.upper() == "K":
+    print("Your weight in pounds is " + str((weight * 2.2)))
+else:
+    print("Invalid input")
