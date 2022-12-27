aa4e55e81714f95dc21c961b22b469b56b699d5d
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-27 10:24:08 +0530

    Adding into list using for loop

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 sum_even = 0
-sum_odd = 0
+
+li_odd = []
+li_even = []
+
 for i in range (1,51,2):
+    
     sum_even += i
-    sum_odd += i+1
-print("Sum of even = " + str(sum_even))
-print("Sum of odd = " + str(sum_odd))
+    li_even.append(i)
+    li_odd.append(i+1)
+
+print(li_even)
+print(li_odd)
