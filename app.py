75c091e3b1d0ca5830b1cf5c16e7ffffa88cc872
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-27 10:18:02 +0530

    Previous quistion simple answer

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 sum_even = 0
 sum_odd = 0
-for i in range (1,51):
-    if i % 2 == 0:
-        sum_even += i
-    else:
-        sum_odd += i
+for i in range (1,51,2):
+    sum_even += i
+    sum_odd += i+1
 print("Sum of even = " + str(sum_even))
 print("Sum of odd = " + str(sum_odd))
