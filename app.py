86ec229c1425c5d2569b74415e286ecb365fadd1
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-12-30 11:00:36 +0530

    Print the number of letters in a word

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-len1 = 4
-len2 = 5
-len3 = 3
-
-
-if len1 > len2 and len1 > len3:
-    if len1 * len1 == len2*len2 + len3*len3:
-        print("Square triangle")
-    else:
-        print("Not a square triangle")
-elif len2 > len3:
-    if len2 * len2 == len1*len1 + len3*len3:
-        print("Square triangle")
-    else:
-        print("Not a square triangle")
-else:
-    if len3 * len3 == len2*len2 + len1*len1:
-        print("Square triangle")
-    else:
-        print("Not a square triangle")
+abc = "python is a simple programming language"
+words = abc.split(" ")
+print(words)
+print(len(words))
+for i in words:
+    print(i + " >" + str(len(i)))
