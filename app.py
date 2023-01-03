1b258517196608970cf7befcf27d270c17208f75
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-01-03 20:46:00 +0530

    Quiz game

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
-abc = "python is a simple programming language"
-words = abc.split(" ")
-print(words)
-print(len(words))
-for i in words:
-    print(i + " >" + str(len(i)))
+print("Welcome to my Game!")
+playing = input("Do you want to play the game? ")
+if playing.upper() != "YES":
+    quit()
+print("Okay! let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.upper() == "CENTRAL PROCESSING UNIT":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.upper() == "GRAPHICS PROCESSING UNIT":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.upper() == "RANDOM ACESS MEMORY":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stands for? ")
+if answer.upper() == "POWER SUPPLY":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("Your score is " + str(score))
+print("Your percentage is " + str((score/4)*100)+"%")
