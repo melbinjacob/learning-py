6ef3f1dac4536f1d26ba1dce723deee3766f04d2
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-01-11 10:13:43 +0530

    List operators

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-fName = input("Enter your first Name: ")
-lName = input("Enter your Last Name: ")
+#Empty list
+myLIst = []
+print(myLIst)
 
-print("Hello " + lName + " " + fName)
+#List of intigers
+myLIst=[1,2,3]
+print(myLIst)
+
+#List with mixed data types
+myLIst=[1,"Hello",4.5]
+print(myLIst)
+
+#Nested list with mixed data types
+myLIst=[2,[2,4,5,4],"Potato"]
+print(myLIst)
