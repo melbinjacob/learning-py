8b023009fe6fab7d650e370d3b8baf9211f3abb8
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-01-23 09:54:26 +0530

    Class question

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-sliceList = [
-    1,2,3,4,5,6,7,8,"Hello",
-    [
-        "Banana","Potato","Cat",
-        ["Boiled Banana","Boiled Potato","Boiled Cat"]
-    ]
-]
+set = {
+    "blue",
+    "green",
+    "red",
+    "black"
+}
 
-print(sliceList[9][3][2:3])
+if "white" in set:
+    print("Yes white is in this set")
+else:
+    print("No")
+
+set.add("yellow")
+print(set)
+
+x = {1,2,3}
+set.update(x)
+print(set)
+
+set.remove("blue")
+print(set)
+
+set.clear()
+print(set)
