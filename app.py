133afc20a7f4663f6c749b00741e2c46b5ab94ae
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-01-25 10:16:08 +0530

    Try expect finally

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-import os
-os.remove("file.x")
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
