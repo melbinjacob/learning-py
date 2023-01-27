3a763c70af13c238666ccb21057f210e289e2939
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-01-27 22:20:26 +0530

    Star pyramid recap

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-class findsum:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-    def addNum(x):
-        sum = x.a + x.b
-        print("Your sum is", sum)
-
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-
-x = findsum(num1,num2)
-x.addNum()
+i = 10
+j = 0
+k = 1
+while(i > 0):
+    print(i * " ", end="")
+    i-=1
+    print(j*"*",end="")
+    j+=1
+    print(k * "*")
+    k+=1
