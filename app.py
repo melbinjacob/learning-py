784de51dd726504982fe8f5122c6930ad56d7ee0
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-02-02 09:33:31 +0530

    Super method

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-i = 10
-j = 0
-k = 1
-while(i > 0):
-    print(i * " ", end="")
-    i-=1
-    print(j*"*",end="")
-    j+=1
-    print(k * "*")
-    k+=1
+class person:
+    def __init__(self) :
+        print("Hello")
+class stud(person):
+    def __init__(self):
+        super().__init__()
+
+p = stud()
+stud()
